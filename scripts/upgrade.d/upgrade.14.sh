c6151415157a1fe8a003134b0575bedaf822294c
dromer <1185977+dromer@users.noreply.github.com>
2020-01-21 12:45:47 +0100

    Update upgrade.14.sh
    use current sha256sum as is used by upstream

--- scripts/upgrade.d/upgrade.14.sh
+++ scripts/upgrade.d/upgrade.14.sh
@@ -4,4 +4,4 @@
 next_version="15.0.0"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="5bb0c58171353da844019b64080c21078002a59ab956ab72adb958844a98eb78"
+nextcloud_source_sha256="f965c14286e7aabbfe49c947d86af59597af302c35d10e0b5440e7e6c53b8f47"
